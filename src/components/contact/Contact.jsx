1a90ba069f4971d46120950b351155828955e86b
Abhidofttech <abhisofttech360@gmail.com>
2024-05-17 18:52:16 +0530

    bug solved in contact page

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import './contact.css'
 
 const Contact = () => {
+    const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "0fc3ab89-c8be-4127-b941-bfeee622ec59");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
     return (
         <>
             <div className="contact">
                 <div className="head">Contact</div>
-                <form action="https://api.web3forms.com/submit" method="POST">
+                <form onSubmit={onSubmit}>
                     <div className="inputBox">
-                    <input type="hidden" name="access_key" value="0fc3ab89-c8be-4127-b941-bfeee622ec59"></input>
                         <div className="formInputContainer">
                             <input type="text" name='Name' className='formInput' placeholder='Name' required />
                         </div>
@@ -20,6 +44,7 @@ const Contact = () => {
                     </div>
                     <button type='submit' className="btn btnLight btnWidth">Submit</button>
                 </form>
+                <span className='result'>{result}</span>
             </div>
         </>
     )
